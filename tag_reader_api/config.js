45d432f5772c703f8c51709c8d60909512808b7a
Daric McFadden <daricmcfadden@Darics-MacBook-Pro.local>
2020-06-10 12:58:28 -0400

    added nodejs api images

--- tag_reader_api/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-    secret : "heymynameismohamedaymen",
-    connectionstring :  { host : 'swm.cwtlz0el2w1c.us-east-1.rds.amazonaws.com', user : 'admin', password : 'N1q2zx90', database : 'swm', connectionLimit: 50, timezone: 'utc', supportBigNumbers: true, bigNumberStrings: true }
-}
